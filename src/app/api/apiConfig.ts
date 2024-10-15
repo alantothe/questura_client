2d2efe0f04d4caae1da73cb848e063c9db01cd45
alantothe <rubenamalpartida@gmail.com>
2024-10-14 23:32:03 -0400

    add getLocation api call

---
+++ src/app/api/apiConfig.ts
@@ -0,0 +1,7 @@
+const apiConfig = {
+    baseUrl: 'http://localhost:4000/api',
+}
+
+export default apiConfig;
+
+
---
+++ src/app/api/get/getLocation.ts
@@ -0,0 +1,18 @@
+import apiConfig  from '../apiConfig'
+
+export default async function getLocation(params:string){
+    
+    try{
+        const response = await fetch(`${apiConfig.baseUrl}/location:${params}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+       const data = await response.json();
+       return data;
+    }catch(error){
+        console.error("Error fetching location:", error);
+        throw error;
+    }
+}
