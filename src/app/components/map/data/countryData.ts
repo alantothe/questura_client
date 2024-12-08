f178f9fc90dc4a6940893e12b6d3a36d1a8b99c8
alantothe <rubenamalpartida@gmail.com>
2024-12-08 14:44:32 -0500

    update countryData.ts

--- src/app/components/map/data/countryData.ts
+++ src/app/components/map/data/countryData.ts
@@ -1,3 +1,7 @@
+type CityMarker = {
+  name: string;
+  coordinates: [number, number];
+};
 
 type CountryData = {
   zoom: number;
@@ -32,7 +36,7 @@ export const countriesMap = new Map<string, CountryData>([
     "Colombia",
     {
       zoom: 12,
-      buttonCoordinates: [-70.07345714315186, 3.9163468591418087],
+      buttonCoordinates: [-67.17345714315186, 8.763468591418087],
       cities: new Map([
         ["Bogotá", [-74.0721, 4.711]],
         ["Medellín", [-75.5636, 6.2518]],
@@ -61,12 +65,12 @@ export const countriesMap = new Map<string, CountryData>([
     "Dominican Rep.",
     {
       zoom: 30,
-      buttonCoordinates: [-66.46185863870569, 18.885420716111305],
+      buttonCoordinates: [-68.46185863870569, 20.885420716111305],
       cities: new Map([["Santo Domingo", [-69.9312, 18.4861]]]),
       returnStyles: {
-        radius: 3,
+        radius: 60,
         strokeWidth: 3,
-        fontSize: 3,
+        fontSize: 32,
       }
     },
   ],
