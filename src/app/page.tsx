ac53563673271a7c41e8d0b04ffcf203b2cbab83
alantothe <rubenamalpartida@gmail.com>
2024-12-13 21:49:35 -0500

    clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import MyMap from "./components/map/MyMap";
-import WorldMap from "./components/map/WorldMap";
 
 export default function Home() {
   return (
     <main>
       <MyMap />
-      {/* <WorldMap /> */}
     </main>
   );
 }
